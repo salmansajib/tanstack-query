fc3edd7e712dcabd0697f4e5917acc120d7cb759
Salman-sajib <sfsajib@gmail.com>
2025-01-11 00:26:13 +0600

    fetched the data with tanstack query

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,46 @@
+import { useQuery } from "@tanstack/react-query";
+
 function App() {
-  return (
-    <main>
-      <div>
-        <h1 className="text-5xl">Hello World!</h1>
+  const { data, status, error } = useQuery({
+    queryKey: ["todo"],
+    queryFn: () =>
+      fetch("https://jsonplaceholder.typicode.com/users/1/todos").then((res) =>
+        res.json(),
+      ),
+  });
+
+  if (status === "pending") {
+    return (
+      <div className="min-h-screen grid place-content-center">
+        <p className="text-2xl">Loading...</p>
       </div>
-    </main>
-  );
+    );
+  }
+
+  if (status === "error") {
+    return (
+      <div className="min-h-screen grid place-content-center">
+        <span className="text-2xl text-red-500">Error: {error.message}</span>
+      </div>
+    );
+  }
+
+  if (status === "success") {
+    return (
+      <main className="min-h-screen grid place-content-center">
+        <div>
+          <h1 className="text-4xl font-bold text-center mb-5 uppercase">
+            Todos
+          </h1>
+          <div>
+            {data?.map((todo) => (
+              <h1 key={todo.id}>{todo.title}</h1>
+            ))}
+          </div>
+        </div>
+      </main>
+    );
+  }
 }
 
 export default App;
