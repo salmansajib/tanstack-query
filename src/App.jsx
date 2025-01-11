e7e71229ef735800adc831f2bba1a5f5deda0192
Salman-sajib <sfsajib@gmail.com>
2025-01-11 12:34:02 +0600

    mutated data with tanstack query

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 
 function App() {
+  const queryClient = useQueryClient();
+
+  // query
   const { data, status, error } = useQuery({
-    queryKey: ["todo"],
+    queryKey: ["posts"],
     queryFn: () =>
-      fetch("https://jsonplaceholder.typicode.com/users/1/todos").then((res) =>
+      fetch("https://jsonplaceholder.typicode.com/posts").then((res) =>
         res.json(),
       ),
+    // staleTime: 4000,
+    refetchInterval: 10000,
+    retry: 5, //the default is 3
+  });
+
+  // mutation
+  const {
+    mutate,
+    status: mutationStatus,
+    error: mutationError,
+  } = useMutation({
+    mutationFn: (newPost) =>
+      fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+        body: JSON.stringify(newPost),
+      }).then((res) => res.json()),
+    onSuccess: (newPost) => {
+      queryClient.setQueryData(["posts"], (oldPost) => [...oldPost, newPost]);
+    },
   });
 
   if (status === "pending") {
@@ -24,17 +49,49 @@ function App() {
       </div>
     );
   }
+  if (mutationStatus === "error") {
+    return (
+      <div className="min-h-screen grid place-content-center">
+        <span className="text-2xl text-red-500">
+          Error: {mutationError.message}
+        </span>
+      </div>
+    );
+  }
 
   if (status === "success") {
     return (
-      <main className="min-h-screen grid place-content-center">
-        <div>
-          <h1 className="text-4xl font-bold text-center mb-5 uppercase">
-            Todos
-          </h1>
-          <div>
-            {data?.map((todo) => (
-              <h1 key={todo.id}>{todo.title}</h1>
+      <main className="min-h-screen bg-[#FDF7F4] grid place-content-center py-5 px-2">
+        <div className="space-y-5">
+          <h1 className="text-4xl font-bold text-center uppercase">Posts</h1>
+          <div className="flex items-center justify-center">
+            <button
+              onClick={() =>
+                // do not need to pass an ID
+                // it will auto generate from the API
+                mutate({
+                  userId: 5000,
+                  title: "New Post",
+                  body: "This is the new post.",
+                })
+              }
+              className="bg-blue-600 px-[30px] py-[10px] text-gray-50 hover:bg-blue-700 transition-colors"
+            >
+              Add Post
+            </button>
+          </div>
+          {mutationStatus === "pending" && <p>Data is being added</p>}
+          <div className="space-y-3">
+            {data?.map((post) => (
+              <div
+                key={post.id}
+                className="w-full max-w-[650px]  p-3 space-y-2 bg-[#BFECFF] rounded-sm shadow-sm"
+              >
+                <h1 className="text-xl text-gray-800 font-medium">
+                  {post.title}
+                </h1>
+                <p className="text-gray-600">{post.body}</p>
+              </div>
             ))}
           </div>
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,9 @@ import "./index.css";
 import App from "./App.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient({});
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { staleTime: 60000, gcTime: 10 * (60 * 1000) } },
+});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
